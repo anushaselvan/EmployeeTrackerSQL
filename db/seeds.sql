f3233dee45b98f06e092cdc237cc544cf6ef4236
anushaselvan <anushaselvan@gmail.com>
2021-11-25 12:39:34 +1100

    Added add department function

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,13 +15,13 @@ VALUES ("HR Coordinator", 70000, 4),
        ("Data Engineer", 80000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id,manager_id)
-VALUES ("Tim", "Geller", 4, 4),
-       ("Raymond", "Holt", 3, 4),
-       ("Jim", "Taylor", 2, 4),
-       ("Channelle", "Jenkins", 1, 4),
-       ("Jake", "Peralta", 5, 2),
-       ("Amy", "Santiago", 7, 2),
-       ("Roza", "Diaz", 6, 4);
+VALUES ("Tim", "Geller", 4), 
+       ("Raymond", "Holt", 3, 1),
+       ("Jim", "Taylor", 2),
+       ("Channelle", "Jenkins", 1, 1),
+       ("Jake", "Peralta", 5, 3),
+       ("Amy", "Santiago", 7,3 ),
+       ("Roza", "Diaz", 6,1);
 
 
        
--- index.js
+++ index.js
@@ -17,64 +17,97 @@ function previewOptions() {
             "View the total utilized budget of a department"
         ]
     }])
-        .then((response) => {
-            let sql = "";
-            let answer = response.preview;
-            switch (answer) {
-                case "View all departments":
-                    sql = 'SELECT * FROM `department`';
-                    mysql2.execute(sql, function (err, results) {
-                        if (err)
-                            throw err;
-                        else
-                            console.table(results);
-                            previewOptions();
-                    });
-                    break;
-                case "View all roles":
-                    sql = 'SELECT * FROM `employeerole`';
-                    mysql2.execute(sql, function (err, results) {
-                        if (err)
-                            throw err;
-                        else
-                            console.table(results);
-                            previewOptions();
-                    });
-                    break;
-                case "View all employees":
-                    sql = 'SELECT * FROM `employee`';
-                    mysql2.execute(sql, function (err, results) {
-                        if (err)
-                            throw err;
-                        else
-                            console.table(results);
-                            previewOptions();
-                    });
-                    break;
-                case "Add a department":
-                case "Add a role":
-                case "Add an employee":
-                case "Update an employee role":
-                case "Update employee managers":
-                case "View employees by manager":
-                case "View employees by department":
-                case "Delete departments, roles, and employees":
-                case "View the total utilized budget of a department":
-
-
-                default:
+    .then((response) => {
+        let sql = "";
+        let answer = response.preview;
+        switch (answer) {
+            case "View all departments":
+                 return viewDepartment(); 
+            case "View all roles":
+                 return viewRole();
+            case "View all employees":
+                 return viewEmployee();
+            case "Add a department":
+                 return addDepartment();
+            case "Add a role":
+                 return addRole();
+            case "Add an employee":
+                 return addEmployee();
+            case "Update an employee role":
+                 return updateEmployeeRole();
+            case "Update employee managers":
+                 return updateEmpByManagers();
+            case "View employees by manager":
+                 return viewEmpByManagers();
+            case "View employees by department":
+                 return viewEmpByDept();
+            case "Delete departments, roles, and employees":
+                 return deleteDeptRoleEmp();
+            case "View the total utilized budget of a department":
+                 return viewBudgetByDept();
+            default:
                     console.log("CMS exited. Bye");
+                    mysql2.end();
 
-            }
+        }
         });
 }
+function viewDepartment(){
+        sql = 'SELECT * FROM department';
+        mysql2.query(sql, function (err, results) {
+            if (err)
+                throw err;
+            else
+                console.table(results);
+                previewOptions();
+            });
+    }
 
+function viewRole(){
+        sql = 'SELECT employeerole.id,employeerole.title, employeerole.salary, department.department_name FROM employeerole INNER JOIN department ON employeerole.department_id= department.id';
+        mysql2.query(sql, function (err, results) {
+            if (err)
+                throw err;
+            else
+                console.table(results);
+                previewOptions();
+        });
+    }
 
+function viewEmployee(){
+        sql = `SELECT employee.id, employee.first_name, employee.last_name, employeerole.title, department.department_name AS department, employeerole.salary, CONCAT(manager.first_name,' ',manager.last_name) AS manager FROM employee LEFT JOIN employeerole ON employee.role_id=employeerole.id LEFT JOIN department ON employeerole.department_id=department.id LEFT JOIN employee manager on manager.id=employee.manager_id `;
+        mysql2.query(sql, function (err, results) {
+            if (err)
+                throw err;
+            else
+                console.table(results);
+                previewOptions();
+        });
+    }
 
+function addDepartment() {
+        inquirer.prompt({
+            type: "input",
+            name: "deptname",
+            message: "What is the name of the department?",
+        })
+        .then((answer) => {
+            sql = 'INSERT INTO department (department_name) VALUES (?)';
 
+            mysql2.query(sql, answer.deptname, function (err, results) {
+                if (err)
+                throw err;
+            else
+            console.log("\n Added department to the database");
+            console.table(results);
+            previewOptions();
+            
+            });
+            });
+    }
 
-const init = () => {
-    previewOptions()
-};
+function init() {
+    previewOptions();
+}
 
 init();
